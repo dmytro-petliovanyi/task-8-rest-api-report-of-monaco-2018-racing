2ca36adfd8cda1228674167c8fd7c50913700ecc
Dmytro Petliovanyi <petliovany@gmail.com>
2023-03-15 16:49:27 +0200

    response rework

--- my_app/resources.py
+++ my_app/resources.py
@@ -7,25 +7,22 @@ from my_app.functions_view import HandleMyData, format_check
 from my_app.my_settings.constants import OrderEnum
 
 
-class ConfigResource(Resource):
-    handle1 = HandleMyData(app.config['TARGET_DIR'])
-    handle = handle1
-
-
-class Report(ConfigResource):
+class Report(Resource):
     @swag_from('swagger/report.yml')
     def get(self) -> Response:
+        handle = HandleMyData(app.config['TARGET_DIR'])
         args = request.args.to_dict()
-        racers_list = self.handle.racers_list_of_dict(self.handle.get_drivers_data())
+        racers_list = handle.racers_list_of_dict(handle.get_drivers_data())
 
         return make_response(format_check(args, racers_list), 200)
 
 
-class Drivers(ConfigResource):
+class Drivers(Resource):
     @swag_from('swagger/drivers.yml')
     def get(self) -> Response:
+        handle = HandleMyData(app.config['TARGET_DIR'])
         args = request.args
-        racers_list = self.handle.get_drivers_data()
+        racers_list = handle.get_drivers_data()
         order_bool = False
 
         if args.get("order"):
@@ -33,24 +30,25 @@ class Drivers(ConfigResource):
 
             order_bool = True if order == OrderEnum.desc else False
 
-        ordered_racers_list = self.handle.sorted_racers(racers_list, order_bool)
-        racers_list_of_dict = self.handle.racers_list_of_dict(ordered_racers_list)
-        num_racers_list_of_dict = self.handle.racers_add_place(racers_list_of_dict)
+        ordered_racers_list = handle.sorted_racers(racers_list, order_bool)
+        racers_list_of_dict = handle.racers_list_of_dict(ordered_racers_list)
+        num_racers_list_of_dict = handle.racers_add_place(racers_list_of_dict)
 
         return make_response(format_check(
             args, num_racers_list_of_dict
         ), 200)
 
 
-class Driver(ConfigResource):
+class Driver(Resource):
     @swag_from("swagger/driver.yml")
     def get(self, driver_id: str) -> Response:
+        handle = HandleMyData(app.config['TARGET_DIR'])
         args = request.args.to_dict()
 
         if driver_id:
-            driver = self.handle.find_racer(driver_id)
+            driver = handle.find_racer(driver_id)
             if driver:
-                driver_dict = self.handle.racer_to_dict(driver)
+                driver_dict = handle.racer_to_dict(driver)
 
                 return make_response(format_check(args, driver_dict), 200)
 
